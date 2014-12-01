5af56810fd00a2f54f7c479c4802b610970825a5
flyingninja314 <drw.ninja@gmail.com>
2014-12-01 14:09:24 -0800

    1 December 2014
    making a database and such

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,14 +4,27 @@
 //    "../" takes you to the folder that your folder is stored in
     $connection = new mysqli($host, $username, $password);
 //    sets $connection as a mysqli object ==> takes in host, username, and password for phpMyAdmin
-    if ($connection->connect_error){
+    if($connection->connect_error) {
 //        try to connect to server
         die('Error: ' . $connection->connect_error);
 //        if there is and error, die and tell user what error is
     }
+    
+    $exists = $connection->select_db($database);
+    
+    if(!$exists) {
+//        checking if we can connect to the database. if the datatbase doesn't exist -> we can create new one
+        $query = $connection->query("CREATE DATABASE $database");
+//        query sent -> create database with name stored in $database
+        
+        if($query) {
+            echo "Succesfully created database " . $database;
+//            if database is created (if query worked) tell me
+        }
+    }
     else {
-        echo "Succes: " . $connection->host_info;
-//        otherwise, say "Success: " and give host info
+        echo "Database already exists.";
+//        if database exists already, tell me
     }
     
     $connection->close();
