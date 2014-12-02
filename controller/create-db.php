d7d50eab7a9377607daeb106ed18c6ba7929cc6a
flyingninja314 <drw.ninja@gmail.com>
2014-12-02 14:02:13 -0800

    2 December 2014
    making a table and such

--- controller/create-db.php
+++ controller/create-db.php
@@ -6,7 +6,7 @@
 //    sets $connection as a mysqli object ==> takes in host, username, and password for phpMyAdmin
     if($connection->connect_error) {
 //        try to connect to server
-        die('Error: ' . $connection->connect_error);
+        die('<p>Error: ' . $connection->connect_error . ".</p> <br>");
 //        if there is and error, die and tell user what error is
     }
     
@@ -18,13 +18,33 @@
 //        query sent -> create database with name stored in $database
         
         if($query) {
-            echo "Succesfully created database " . $database;
+            echo "<p>Succesfully created database " . $database . ".</p> <br>";
 //            if database is created (if query worked) tell me
         }
     }
     else {
-        echo "Database already exists.";
+        echo "<p>Database already exists.</p> <br>";
 //        if database exists already, tell me
+    }
+    
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id))");
+//    re-use $query 
+//    id -> integer, auto increment, can hold 11 spaces (up to 99999999999 posts), can't be empty
+//    title -> 255 characters allowed, not null
+//    post -> chunk of text, not null
+//    primary key -> makes id primary key
+    
+    if($query) {
+        echo "<p>Succesfully created table: posts</p>";
+//        if query works, tell me
+    }
+    else {
+        echo "$connection->error";
+//        if not, tell me why not
     }
     
     $connection->close();
