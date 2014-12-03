ccb90e65e4c585ce9c7e44133a92de7828b175da
flyingninja314 <drw.ninja@gmail.com>
2014-12-03 14:00:13 -0800

    3 December 2014
    view stuff

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            require_once(__DIR__ . '/controller/create-db.php');
-        ?>
-    </body>
-</html>
+<?php
+    require_once (__DIR__ . '/view/header.php');
+    require_once(__DIR__ . '/controller/create-db.php');
+    require_once (__DIR__ . '/view/footer.php');    
+//    now we have reusabe bits of code (header.php and footer.php
+?>
